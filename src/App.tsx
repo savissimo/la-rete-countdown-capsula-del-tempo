cc9ffd0bedc9375b05e7c4947eb31a7e4ff03f7b
savissimo <simone.saviolo@gmail.com>
2024-05-02 01:00:07 +0200

    Funzionalità di base: timer countdown

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useState } from 'react'
+import { DateTime, Duration, Interval } from 'luxon'
+import './App.css'
+
+function App() {
+  const [timeRemaining, setTimeRemaining] = useState<Duration>(Duration.fromMillis(0))
+
+  const updateTimer = useCallback(() => {
+		const finalDate = DateTime.fromISO('2034-07-02T09:00:00+02:00')
+		const now = DateTime.now()
+		const dateInterval = Interval.fromDateTimes(now, finalDate)
+		setTimeRemaining(dateInterval.toDuration([ 'years', 'months', 'days', 'hours', 'minutes', 'seconds', 'milliseconds' ]))
+	}, [])
+
+  useEffect(() => {
+	updateTimer()
+	window.setInterval(updateTimer, 500)
+  }, [updateTimer])
+
+  return <>
+    <header></header>
+	<section>
+		{timeRemaining.isValid
+		? <div id="timer-countdown">
+			<div className="date">
+				<div className="labeled-display years">{timeRemaining?.get('years')}</div>
+				<div className="labeled-display months">{timeRemaining?.get('months')}</div>
+				<div className="labeled-display days">{timeRemaining?.get('days')}</div>
+			</div>
+			<div className="time">
+				<div className="labeled-display hours">{timeRemaining?.get('hours')}</div>
+				<div className="labeled-display minutes">{timeRemaining?.get('minutes')}</div>
+				<div className="labeled-display seconds">{timeRemaining?.get('seconds')}</div>
+			</div>
+		</div>
+		: <div>La capsula del tempo è pronta per essere aperta!</div>
+		}
+	</section>
+  </>
+}
+
+export default App
